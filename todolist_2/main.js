088b567a25aafa58db0a2de4277790d21813779e
mijin.sung <mijin.sung09@gmail.com>
2024-02-14 21:28:49 +0900

    TODOLIst - 3차

--- todolist_2/main.js
+++ todolist_2/main.js
@@ -2,43 +2,10 @@
 
 const task_text = document.querySelector('.input-text');
 const btn_add = document.querySelector('.button-plus');
-const no_data = document.querySelector('.no-data');
 const task_list = document.querySelector('.tasks');
 const tab_wrap = document.querySelector('.tabs');
 const tabs = tab_wrap.querySelectorAll('button');
 let taskList = [];
-let filteredList = [];
-let mode = 'all';
-
-const filterList = function(e){
-  mode = e.target.dataset.mode;
-  filteredList = [];
-
-  if(mode === "all"){
-    renderTask();
-  } else if(mode === "doing"){
-    for(let i = 0; i < taskList.length ; i++){
-      if(taskList[i].isComplete == false){
-        filteredList.push(taskList[i]);
-      }
-    }
-    renderTask();
-  } else if(mode === "done"){
-    for(let i = 0; i < taskList.length ; i++){
-      if(taskList[i].isComplete == true){
-        filteredList.push(taskList[i]);
-      }
-    }
-    renderTask();
-  }
-}
-
-for(let i = 0; i < tabs.length ; i++){
-  tabs[i].addEventListener('click', (e) => { console.log(e);
-    tab_wrap.style.setProperty('--underline-left', `${e.target.offsetLeft}px`);
-    filterList(e);
-  });
-}
 
 const generateId = function(){
   return '_' + Math.random().toString(36).substr(2, 9);
@@ -62,14 +29,6 @@ const removeItem = function(id){
       break;
     }
   }
-  if( filteredList.length !== 0 ){
-    for(let i=0 ; i < filteredList.length ; i++){
-      if(filteredList[i].id == id){
-        filteredList.splice(i, 1);
-        break;
-      }
-    }
-  }
   renderTask();
 }
 
@@ -89,35 +48,22 @@ const addTask = function(){
 
 const renderTask = function(){ 
   let listHTML = '';
-  let list = [];
-  
-  if(mode === "all"){
-    list = taskList;
-  } else {
-    list = filteredList;
-  }
 
-  if( list.length !== 0){
-    no_data.style.display = 'none';
-  } else {
-    no_data.removeAttribute('style');
-  }
-
-  for(let i = 0; i < list.length ; i++){
-    if( list[i].isComplete == true ){
+  for(let i = 0; i < taskList.length ; i++){
+    if( taskList[i].isComplete == true ){
       listHTML += `
       <li class="task is-checked">
-        <button type="button" class="button-check" onclick="toggleComplete('${list[i].id}')"><span class="visually-hidden">check</span></button>
-        <p class="content">${list[i].content}</p>
-        <button type="button" class="button-delete" onclick="removeItem('${list[i].id}')"><span class="visually-hidden">delete</span></button>
+      <p class="content">${taskList[i].content}</p>
+        <button type="button" class="button-check" onclick="toggleComplete('${taskList[i].id}')">check</button>
+        <button type="button" class="button-delete" onclick="removeItem('${taskList[i].id}')"><span class="visually-hidden">delete</span>🗑</button>
       </li>
       `;
     } else {
       listHTML += `
       <li class="task">
-        <button type="button" class="button-check" onclick="toggleComplete('${list[i].id}')"><span class="visually-hidden">check</span></button>
-        <p class="content">${list[i].content}</p>
-        <button type="button" class="button-delete" onclick="removeItem('${list[i].id}')"><span class="visually-hidden">delete</span></button>
+      <p class="content">${taskList[i].content}</p>
+        <button type="button" class="button-check" onclick="toggleComplete('${taskList[i].id}')">check</button>
+        <button type="button" class="button-delete" onclick="removeItem('${taskList[i].id}')"><span class="visually-hidden">delete</span>🗑</button>
       </li>
       `;
     }

8c608c067d6a68a431727c4f8dd369f5a171513e
mijin.sung <mijin.sung09@gmail.com>
2024-02-21 14:59:12 +0900

    API

--- api/main.js
+++ api/main.js
@@ -1,7 +1,7 @@
 let newList = [];
 
 const getLatesNews = async () => {
-  const url = new URL('https://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines');
+  const url = new URL('https://noona-times-v2.netlify.app/top-headlines');
   const response = await fetch(url);
   const data = await response.json();
   newsList = data.articles; console.log('newslist', newsList);

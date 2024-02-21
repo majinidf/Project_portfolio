7fc7b46496f776aa9ec97dc4763a4bb2f8b8ff0a
mijin.sung <mijin.sung09@gmail.com>
2024-02-21 14:30:28 +0900

    API

--- api/main.js
+++ api/main.js
@@ -1,7 +1,9 @@
+let newList = [];
+
 const getLatesNews = async () => {
   const url = new URL('https://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines');
-  const response = await fetch(url);  console.log('response ', response);
-  const data = await response.json();  console.log(data);
+  const response = await fetch(url);
+  const data = await response.json();
   newsList = data.articles; console.log('newslist', newsList);
 }
 
